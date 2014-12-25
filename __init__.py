cd4ce3c3a695894500d95f337cc729e12d7053b9
aviralg <aviral.goel@outlook.com>
2014-12-25 12:18:33 +0530

    Disabled import warnings from pylint.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 import sys
 import traceback
+from goose.config import *
 
-def exceptionHook(exception_class, exception, tb):
+def excepthook(exception_class, exception, tb):
     CRITICAL('{0}: {1}'.format(exception_class.__name__, exception))
     CRITICAL("\n" + "".join(traceback.format_tb(tb)))
     sys.exit()
 
-sys.excepthook = exceptionHook
+sys.excepthook = excepthook
